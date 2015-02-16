a0449fcf1812e9bddfb2ca6bb84aafd029732548
burdiyan <burdiyan@gmail.com>
2015-02-16 10:48:50 +0100

    Venv aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias homest="~/Development/Homestead/Projects"
 alias codecept="./vendor/bin/codecept"
 alias phpunit="./vendor/bin/phpunit"
+alias venv="virtualenv"
+alias venva="source bin/activate"
+
