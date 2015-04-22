d45a0651c27ec96914d45f1067e07a35ddb79c79
burdiyan <burdiyan@gmail.com>
2015-04-22 09:39:01 +0200

    Adding more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,7 @@ alias codecept="./vendor/bin/codecept"
 alias phpunit="./vendor/bin/phpunit"
 alias venv="virtualenv"
 alias venva="source bin/activate"
+alias art="php artisan"
+alias pipupdate="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+alias gf="git fetch"
 
