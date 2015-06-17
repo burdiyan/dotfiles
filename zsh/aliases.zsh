03cee8a7cd36f59920aa08e659c8a6445d8f57f3
burdiyan <burdiyan@gmail.com>
2015-06-17 22:27:27 +0200

    Adding alias for development directory

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,4 +6,5 @@ alias art="php artisan"
 alias pipupdate="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 alias gf="git fetch"
 alias gpp="git push && git push"
+alias godev="cd ~/Development"
 
