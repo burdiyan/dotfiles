977963874a2dc6461f9f3c70a6e0b66c1f738f67
Alexandr Burdiyan <burdiyan@gmail.com>
2015-08-22 12:03:19 +0200

    Changed venv alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,11 @@
 alias codecept="./vendor/bin/codecept"
 alias phpunit="./vendor/bin/phpunit"
 alias venv="virtualenv"
-alias venva="source bin/activate"
+alias venva="source venv/bin/activate"
 alias art="php artisan"
 alias pipupdate="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 alias gf="git fetch"
 alias gpp="git push && git push"
 alias godev="cd ~/Development"
+alias django="python3 manage.py"
 
