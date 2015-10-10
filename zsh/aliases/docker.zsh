cc99043e69a403d233899db2bb347262b8b94303
Alexandr Burdiyan <burdiyan@gmail.com>
2015-10-11 01:26:01 +0200

    more docker aliases

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -1,2 +1,9 @@
 alias dma="docker-machine"
 alias dco="docker-compose"
+alias dim="docker images"
+alias dst="docker stop"
+
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias drm="docker rm"
+alias drmi="docker rmi"
